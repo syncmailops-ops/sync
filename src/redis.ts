60c80715091b487d6dd67d567d1f2e5205ca2f47
syncmailops-ops <sync.mail.ops@gmail.com>
2026-03-06 06:02:28 +0530

    Update redis.ts

--- src/redis.ts
+++ src/redis.ts
@@ -1,5 +1,12 @@
 import Redis from "ioredis";
 
-const redis = new Redis(process.env.REDIS_URL || "");
+let redis: Redis | null = null;
 
-export default redis;
+export function getRedis() {
+  if (!redis) {
+    redis = new Redis(process.env.REDIS_URL || "");
+  }
+  return redis;
+}
+
+export default getRedis;
